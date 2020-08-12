6bf8d728047ed68c27e43bc8be5e6442e65ec1b3
David Keller <mrcodeswildride@gmail.com>
2020-08-12 21:56:07 +0000

    es6 update

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # wildcards
 
-This is a wildcards web app written in HTML, CSS, and JavaScript.
+Wildcards web app written in HTML, CSS, and JavaScript.
 
 Use this app at [wildcards.mrcodeswildride.com](https://wildcards.mrcodeswildride.com/).
 Learn how to make this app at [mrcodeswildride.com](https://www.mrcodeswildride.com/).
--- script.js
+++ script.js
@@ -1,189 +1,166 @@
-var lettersInput = document.getElementById("letters");
-var findWordsButton = document.getElementById("findWords");
-var showAllDiv = document.getElementById("showAllDiv");
-var showAllCheckbox = document.getElementById("showAll");
-var messageParagraph = document.getElementById("message");
-var wordsDiv = document.getElementById("words");
+let letters = document.getElementById(`letters`)
+let wordsButton = document.getElementById(`wordsButton`)
+let showAllDiv = document.getElementById(`showAllDiv`)
+let showAllCheckbox = document.getElementById(`showAllCheckbox`)
+let box = document.getElementById(`box`)
 
-var dict = {};
-var wordCombos;
-var wordCount;
+let finding = false
+let words
 
-lettersInput.addEventListener("keydown", lettersInputKeydown);
-findWordsButton.addEventListener("click", findWords);
-showAllCheckbox.addEventListener("change", toggleCombos);
+let dictionary = {}
+fetchDictionary()
 
-lettersInput.focus();
-fetchDict();
+wordsButton.addEventListener(`click`, startFindWords)
+showAllCheckbox.addEventListener(`change`, toggleShowAll)
 
-function lettersInputKeydown(event) {
-    if (event.keyCode == 13 && !findWordsButton.disabled) {
-        findWords();
+letters.addEventListener(`keydown`, keyPressed)
+letters.focus()
+
+function startFindWords() {
+  let lettersValue = letters.value.trim().toLowerCase()
+
+  if (lettersValue != `` && !finding) {
+    showAllCheckbox.checked = false
+    let numWildcards = getNumWildcards(lettersValue)
+
+    if (numWildcards > 5) {
+      box.innerHTML = `Input cannot have more than 5 wildcards.`
+      showHideCheckbox(numWildcards)
     }
+    else {
+      box.innerHTML = `Finding all combinations...`
+      finding = true
+
+      setTimeout(findWords, 10, lettersValue, numWildcards)
+    }
+  }
+
+  letters.focus()
 }
 
-function findWords() {
-    let letters = lettersInput.value.trim().toLowerCase();
-
-    if (letters != "") {
-        let numWildcards = getNumWildcards(letters);
-
-        showAllDiv.classList.add("hidden");
-        wordsDiv.innerHTML = "";
-
-        if (numWildcards <= 5) {
-            findWordsButton.disabled = true;
-            messageParagraph.innerHTML = "Finding all combinations...";
-
-            setTimeout(function() {
-                let startTime = Date.now();
-                wordCombos = getWordCombos(letters);
-
-                let noun = (wordCombos.length == 1) ? "combination" : "combinations";
-                let seconds = (Date.now() - startTime) / 1000;
-                messageParagraph.innerHTML = "Found " + wordCombos.length + " " + noun + " in " + seconds.toFixed(3) + " seconds, now finding actual words...";
-
-                setTimeout(function() {
-                    wordCount = 0;
-
-                    for (let i = 0; i < wordCombos.length; i++) {
-                        if (dict[wordCombos[i]]) {
-                            let wordDiv = document.createElement("div");
-                            wordDiv.classList.add("word");
-                            wordDiv.classList.add("valid");
-                            wordDiv.innerHTML = wordCombos[i];
-                            wordsDiv.appendChild(wordDiv);
-                            wordCount++;
-                        }
-                    }
-
-                    findWordsButton.disabled = false;
-
-                    if (numWildcards <= 3) {
-                        showAllCheckbox.checked = false;
-                        showAllDiv.classList.remove("hidden");
-                    }
-
-                    noun = (wordCount == 1) ? "word" : "words";
-                    seconds = (Date.now() - startTime) / 1000;
-                    messageParagraph.innerHTML = "Found " + wordCount + " " + noun + ". Total time: " + seconds.toFixed(3) + " seconds";
-                }, 10);
-            }, 10);
-        }
-        else {
-            messageParagraph.innerHTML = "Input cannot have more than 5 wildcards.";
-        }
-    }
-}
-
-function toggleCombos() {
-    findWordsButton.disabled = true;
-    showAllCheckbox.disabled = true;
-    wordsDiv.innerHTML = "";
-
+function toggleShowAll() {
+  if (!finding) {
     if (showAllCheckbox.checked) {
-        messageParagraph.innerHTML = "Showing all combinations...";
+      box.innerHTML = `Showing all combinations...`
+      finding = true
+
+      setTimeout(showWords, 10)
     }
     else {
-        messageParagraph.innerHTML = "Showing actual words only...";
+      showWords()
     }
-
-    setTimeout(function() {
-        for (let i = 0; i < wordCombos.length; i++) {
-            if (showAllCheckbox.checked || dict[wordCombos[i]]) {
-                let wordDiv = document.createElement("div");
-                wordDiv.classList.add("word");
-                wordDiv.innerHTML = wordCombos[i];
-                wordsDiv.appendChild(wordDiv);
-
-                if (dict[wordCombos[i]]) {
-                    wordDiv.classList.add("valid");
-                }
-            }
-        }
-
-        findWordsButton.disabled = false;
-        showAllCheckbox.disabled = false;
-
-        if (showAllCheckbox.checked) {
-            let noun = (wordCombos.length == 1) ? "combination" : "combinations";
-            messageParagraph.innerHTML = "Showing " + wordCombos.length + " " + noun + ".";
-        }
-        else {
-            let noun = (wordCount == 1) ? "word" : "words";
-            messageParagraph.innerHTML = "Showing " + wordCount + " " + noun + ".";
-        }
-    }, 10);
+  }
 }
 
-function getNumWildcards(letters) {
-    var numWildcards = 0;
+function findWords(lettersValue, numWildcards) {
+  words = [``]
 
-    for (var i = 0; i < letters.length; i++) {
-        if (letters[i] == "?") {
-            numWildcards++;
-        }
+  for (let character of lettersValue) {
+    let combos = getCombos(character)
+    let newWords = []
+
+    for (let word of words) {
+      for (let combo of combos) {
+        newWords.push(`${word}${combo}`)
+      }
     }
 
-    return numWildcards;
+    words = newWords
+  }
+
+  showHideCheckbox(numWildcards)
+  showWords()
 }
 
-function getWordCombos(input) {
-    if (input.length == 1) {
-        if (input != "?") {
-            return [input];
-        }
-        else {
-            var letters = [];
+function getCombos(character) {
+  if (character == `?`) {
+    let alphabet = []
 
-            for (var charCode = 97; charCode <= 122; charCode++) {
-                var letter = String.fromCharCode(charCode);
-                letters.push(letter);
-            }
-
-            return letters;
-        }
+    for (let i = 97; i <= 122; i++) {
+      alphabet.push(String.fromCharCode(i))
     }
-    else {
-        var allButLastLetter = input.substring(0, input.length - 1);
-        var lastLetter = input[input.length - 1];
 
-        var subwords = getWordCombos(allButLastLetter);
-        var combos = [];
-
-        for (var i = 0; i < subwords.length; i++) {
-            var subword = subwords[i];
-            var combo;
-
-            if (lastLetter != "?") {
-                combo = subword + lastLetter;
-                combos.push(combo);
-            }
-            else {
-                var lastLetters = getWordCombos(lastLetter);
-
-                for (var j = 0; j < lastLetters.length; j++) {
-                    combo = subword + lastLetters[j];
-                    combos.push(combo);
-                }
-            }
-        }
-
-        return combos;
-    }
+    return alphabet
+  }
+  else {
+    return character
+  }
 }
 
-function fetchDict() {
-    var request = new XMLHttpRequest();
-    request.open("GET", "dictionary.txt");
-    request.addEventListener("load", createDict);
-    request.send();
+function showHideCheckbox(numWildcards) {
+  if (numWildcards <= 3) {
+    showAllDiv.style.display = `inline-block`
+  }
+  else {
+    showAllDiv.style.display = `none`
+  }
 }
 
-function createDict() {
-    var words = this.response.split("\n");
+function showWords() {
+  box.innerHTML = ``
+  let wordsFound = false
 
-    for (var i = 0; i < words.length; i++) {
-        dict[words[i]] = true;
+  for (let word of words) {
+    if (dictionary[word] || showAllCheckbox.checked) {
+      let wordDiv = makeWordDiv(word)
+
+      if (dictionary[word]) {
+        labelValid(wordDiv)
+      }
+
+      wordsFound = true
     }
+  }
+
+  if (!wordsFound) {
+    box.innerHTML = `No words found.`
+  }
+
+  finding = false
+}
+
+function makeWordDiv(word) {
+  let wordDiv = document.createElement(`div`)
+  wordDiv.classList.add(`word`)
+  wordDiv.innerHTML = word
+  box.appendChild(wordDiv)
+
+  return wordDiv
+}
+
+function labelValid(wordDiv) {
+  wordDiv.classList.add(`valid`)
+}
+
+function getNumWildcards(lettersValue) {
+  let numWildcards = 0
+
+  for (let character of lettersValue) {
+    if (character == `?`) {
+      numWildcards++
+    }
+  }
+
+  return numWildcards
+}
+
+function fetchDictionary() {
+  let request = new XMLHttpRequest()
+  request.addEventListener(`load`, makeDictionary)
+  request.open(`GET`, `dictionary.txt`)
+  request.send()
+}
+
+function makeDictionary() {
+  let words = this.response.split(`\n`)
+
+  for (let word of words) {
+    dictionary[word] = true
+  }
+}
+
+function keyPressed(event) {
+  if (event.keyCode == 13) {
+    startFindWords()
+  }
 }
